a8f71af220c8642474212f702538a9db8a3203b7
Supriya S <supriyas@practo.com>
2018-08-14 17:22:22 +0530

    changes to demo file

--- demo/src/components/JSXExpressionStyleResolution.js
+++ demo/src/components/JSXExpressionStyleResolution.js
@@ -3,10 +3,9 @@ import classnames from 'classnames';
 import './table.css';
 
 export default () => {
-  var cname;
-  cname = classnames({'row': true});
+  var cname = classnames({'row': true});
 
-  return <div className="kjdhf" styleName={classnames('table')}>
+  return <div styleName={classnames('table')}>
     <div styleName={cname}>
       <div styleName='cell'>A2</div>
       <div styleName='cell'>B2</div>
